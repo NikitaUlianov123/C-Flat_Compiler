8d253fc52ef835ed27ee8742ed0466b0d37512f2
Nikita Ulianov <nikita@mectruck.com>
2025-06-20 01:14:21 -0700

    basic parsing works, will improve tommorow

--- Compiler/Compiler/Parser.cs
+++ Compiler/Compiler/Parser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
@@ -11,14 +12,106 @@ namespace Compiler
 {
     public static class Parser
     {
-        private static readonly Dictionary<IToken, List<int>> ParseNodes = new()
+        private static readonly Dictionary<Type, List<List<Type>>> ParseNodes = new()
         {
-            //[WhiteSpace] = [0],
+            [typeof(Program)] = [[typeof(PrintStatement)],
+                                 [typeof(VariableExpr)]],
+            [typeof(PrintStatement)] = [[typeof(PrintKeyword), typeof(OpenParenthesis), typeof(StringValue), typeof(CloseParenthesis), typeof(Semicolon)]],//print("hello")
+
+            [typeof(VariableExpr)] = [[typeof(Identifier), typeof(Identifier), typeof(Semicolon)],//int a;
+                                      [typeof(Identifier), typeof(Identifier), typeof(AssignmentOperator), typeof(MathExpr), typeof(Semicolon)],//int a = 5;
+                                      [typeof(Identifier), typeof(AssignmentOperator), typeof(MathExpr), typeof(Semicolon)]],//a = 2;
+
+            [typeof(MathExpr)] = [[typeof(Identifier)],
+                                  [typeof(NumericValue)]],
         };
 
-        public static void Parse(List<IToken> tokens)
+        public static List<string> Parse(List<IToken> tokens, out ParseNode? root)
         {
+            List<string> messages = [];
+            root = new ParseNode();
 
+            while (tokens.Count > 0)
+            {
+                var newKid = parse(typeof(Program), tokens, messages);
+
+                if (newKid == null)//there was a problem
+                {
+                    while (tokens[0] is not Semicolon)//panic mode
+                    {
+                        tokens.RemoveAt(0);
+                    }
+                    tokens.RemoveAt(0);
+                }
+                else
+                { 
+                    root.Children.Add(newKid);
+                }
+            }
+            return messages;
+            
+
+            ParseNode? parse(Type nonTerminal, List<IToken> tokens, List<string> messages)
+            {
+                if (ParseNodes.ContainsKey(nonTerminal))
+                {
+                    foreach (var production in ParseNodes[nonTerminal])
+                    {
+                        if (tokens.Count < production.Count) continue;
+
+                        ParseNode result = (ParseNode)Activator.CreateInstance(nonTerminal)!;
+
+                        List<IToken> tempList = tokens.Select(x => x).ToList();//List.Copy yo!
+
+                        bool success = true;
+                        for (int i = 0; i < production.Count; i++)
+                        {
+                            if (typeof(IToken).IsAssignableFrom(production[i]))//child is IToken(terminal)
+                            {
+                                if (tempList[0].GetType() == production[i])//and the next token is what we want
+                                {
+                                    result.Children.Add(tempList[0]);
+                                    tempList.RemoveAt(0);
+                                }
+                                else
+                                {
+                                    success = false;
+                                    break;
+                                }
+                            }
+                            else
+                            {
+                                var newKid = parse(production[i], tempList, messages);
+                                if (newKid == null)
+                                { 
+                                    success = false;
+                                    break;
+                                }
+                                result.Children.Add(newKid);
+                            }
+                        }
+
+                        if (success)
+                        {
+                            while (tokens.Count > tempList.Count)
+                            { 
+                                tokens.RemoveAt(0);
+                            }
+                            return result;
+                        }
+                    }
+                }
+
+                if (nonTerminal is IToken)
+                {
+                    messages.Add("non-terminal is actually terminal, wtf?");
+                }
+                else
+                {
+                    messages.Add($"No productions for {nonTerminal.Name}. row {tokens[0].Row}, column {tokens[0].Column}");
+                }
+                return null;
+            }
         }
 
         public static void NukeWhiteSpace(List<IToken> tokens)
@@ -34,147 +127,16 @@ namespace Compiler
         }
     }
 
-    public abstract class ParseNode
+
+    //[DebuggerDisplay("{this.GetType().Name}")]
+    public record class ParseNode
     {
-        public List<ParseNode> Tokens = [];
-        //public static virtual bool DoesParse(List<IToken> tokens, int index, out int newIndex, List<string> messages, [NotNullWhen(true)] out ParseNode? parseNode);
+        public List<object> Children = [];//i know this is bad, it's (hopefully) temporary
     }
 
-    public class Terminal : ParseNode
-    {
-        public IToken Token { get; private set; }
 
-        public Terminal(IToken token)
-        {
-            Token = token ?? throw new ArgumentNullException(nameof(token));
-        }
-    }
-
-    public class Program : ParseNode
-    {
-        public Program(List<ParseNode> tokens)
-        {
-            Tokens = tokens;
-        }
-
-        public static bool DoesParse(List<IToken> tokens, int index, out int newIndex, List<string> messages, [NotNullWhen(true)] out ParseNode? parseNode)
-        {
-            void missingMessage(char character, int tokenIndex)
-            { 
-                messages.Add($"Missing '{character}'. row:{tokens[index + tokenIndex].Row}, column:{index + tokens[tokenIndex].Column}.");
-            }
-            void message(string message, int tokenIndex)
-            {
-                messages.Add($"{message}. row:{tokens[tokenIndex].Row}, column:{tokens[tokenIndex].Column}.");
-            }
-
-            //print
-            if (tokens[index] is PrintKeyword && tokens.Count > index + 4)
-            {
-                //(
-                if (tokens[index + 1] is OpenParenthesis)
-                {
-                    //string
-                    if (tokens[index + 2] is StringValue)
-                    {
-                        //)
-                        if (tokens[index + 3] is CloseParenthesis)
-                        {
-                            //;
-                            if (tokens[index + 4] is Semicolon)
-                            {
-                                newIndex = index + 5;
-                                parseNode = new Program([new Terminal(tokens[index]),
-                                                         new Terminal(tokens[index + 1]),
-                                                         new Terminal(tokens[index + 2]),
-                                                         new Terminal(tokens[index + 3]),
-                                                         new Terminal(tokens[index + 4])]);
-                                return true;
-                            }
-                            else missingMessage(';', 3);
-                        }
-                        else missingMessage(')', 2);
-                    }
-                    else message("Invalid value, not a string literal.", 1);
-                }
-                else missingMessage('(', 0);
-            }
-            if (tokens[index] is IfKeyword)
-            {
-                throw new NotImplementedException("If statements are not yet implemented.");
-            }
-            if (tokens[index] is ElseKeyword)
-            {
-                throw new NotImplementedException("Else statements are not yet implemented.");
-            }
-            if (tokens[index] is Identifier && tokens.Count > index + 3)
-            {
-                //variable declaration
-                if (tokens[index + 1] is Identifier)
-                {
-                    //int a = something;
-                    if (tokens[index + 2] is AssignmentOperator)
-                    {
-                        if (MathExpr.DoesMatch(tokens, index + 3, out int tempIndex, messages, out MathExpr? result))
-                        {
-
-                        }
-                        else
-                        { 
-                            
-                            return false;
-                        }
-                    }
-
-                    //int a;
-                    if (tokens[index + 2] is Semicolon)
-                    {
-                        newIndex = index + 3;
-                        parseNode = new Program([new Terminal(tokens[index]),
-                                                 new Terminal(tokens[index + 1]),
-                                                 new Terminal(tokens[index + 2])]);
-                        return true;
-                    }
-                    else missingMessage(';', 2);
-                }
-                //variable assignment
-                if (tokens[index + 1] is AssignmentOperator)
-                {
-                    throw new NotImplementedException("can't assign variables yet");
-                }
-            }
-            else //nothing matched
-            {
-                message("Invalid start of line", 0);
-                newIndex = 0;
-                parseNode = null;
-                return false;
-            }
-        }
-    }
-
-    public class MathExpr : ParseNode
-    {
-        public MathExpr(List<ParseNode> tokens)
-        {
-            Tokens = tokens;
-        }
-
-        public static bool DoesMatch(List<IToken> tokens, int index, out int newIndex, List<string> messages, [NotNullWhen(true)] out MathExpr? parseNode)
-        {
-            if (tokens[index] is NumericValue or Identifier)
-            {
-                newIndex = index + 1;
-                parseNode = new MathExpr([new Terminal(tokens[index])]);
-                return true;
-            }
-            else
-            {
-                messages.Add($"Invalid expression. row:{tokens[index].Row}, column:{tokens[index].Column}.");
-                newIndex = 0;
-                parseNode = null;
-                return false;
-            }
-        }
-    }
+    public record class Program() : ParseNode;
+    public record class MathExpr : ParseNode;
+    public record class VariableExpr : ParseNode;
+    public record class PrintStatement : ParseNode;
 }
--- Compiler/TestProject/Test1.cs
+++ Compiler/TestProject/Test1.cs
@@ -13,14 +13,15 @@ namespace TestProject
         [TestMethod]
         public void TestMethod1()
         {
-            bool success = Lexer.Lex("print(\"Hello\");", out var result);
+            bool success = Lexer.Lex("print(\"Hello\");\nint a = 2;\nb = ;", out var result);
             Assert.IsTrue(success);
 
-            success = Lexer.Lex("int a = 7;", out result);
-            Assert.IsTrue(success);
+            //success = Lexer.Lex("int a = 7;", out result);
+            //Assert.IsTrue(success);
 
             Parser.NukeWhiteSpace(result);
 
+            var parseTree = Parser.Parse(result, out var tree);
             ;
         }
     }
